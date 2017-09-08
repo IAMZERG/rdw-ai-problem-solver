54f0f73c9b65997599c9ab2f9160adaf9e3140bb
Wes Wright <inventor487@gmail.com>
2017-09-08 00:32:57 -0400

    add boardstate class, work on tap_lands function.  non-functional commit

--- tap_lands.js
+++ tap_lands.js
@@ -24,12 +24,62 @@ module.exports.tap_lands = function (player_boardstate, manacost) {
 //create boardstate to handle all of this with helper functions?
 
 class Boardstate {
-  constructor(theStack = [], activePlayer = ""p1 = {}, p2 = {}) {
+  constructor(theStack = [], p1 = {}, p2 = {}, activePlayer = "") {
     this.p1 = p1;
     this.p2 = p2;
     this.theStack = theStack;
     this.activePlayer = activePlayer;
   }
+  this.getPlayer(num) {
+    if(num === 1) {
+      return this.p1;
+    } else if (num === 2) {
+      return this.p2;
+    } else {
+      return 0;
+    }
+  }
+  this.find_lands(p_boardstate, mana_type) {
+    
+    return p_board.battlefield.map((card) => {
+      if( card.tapsFor.indexOf(mana_type) !== -1 && card.state === "untapped" ) {
+        return true;
+      }
+      return false;
+    });
+  }
+
+  this.tap_lands(generic = 0, white = 0, blue = 0, black = 0, red = 0, green = 0, colorless = 0, player = 1) {
+    var player_board = this.getPlayer(player);
+    var generic_lands = this.find_lands(player_board, "generic");
+    var white_lands = this.find_lands(player_board, "white");
+    var blue_lands = this.find_lands(player_board, "blue");
+    var black_lands = this.find_lands(player_board, "black");
+    var red_lands = this.find_lands(player_board, "red");
+    var green_lands = this.find_lands(player_board, "green");
+    var colorless_lands = this.find_lands(player_board, "colorless");
+
+    var land_arr = [generic_lands, white_lands, blue_lands, black_lands, red_lands, green_lands, colorless_lands];
+
+    land_arr = land_arr.sort((a,b) => {
+      if (a.length < b.length) {
+        return -1;
+      }
+      if (a.length > b.length) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    // take sorted array, loop through them tapping the necessary sources
+    //return 0 if an issue is encountered (i.e. not enough sources
+    //nest all of this inside a try/catch loop.
+  }
+
+    
+
+
 }
 
 module.exports.Boardstate;
